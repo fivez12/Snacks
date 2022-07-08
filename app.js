016b8fe50f2c8a35639fa2e217c2318d367a0dff
fivez <fivezzzcarpine@gmail.com>
2022-07-08 21:38:44 +0300

    Test commit

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 function randomImg() {
     var quotes = [
       {
-        text: "BIO1",
-        img:  "https://lays.ro/images/pack-clasic-sare.png"
+        price:360,
+        kcal:350,
+        img:  "https://lays.ro/images/pack-clasic-sare.png",
       },
-      {
-        text: "BIO2",
+      {price:360,
+        kcal:350,
         img:  "https://lays.ro/images/pack-clasic-barbecue.png",
       },
-      {
-        text: "BIO3",
+      {price:360,
+        kcal:350,
         img:  "https://lays.ro/images/pack-clasic-onion.png",
       },
-      {
-        text: "BIO4",
+      {price:360,
+        kcal:350,
         img:  "https://lays.ro/images/pack-clasic-paprika.png",
       },
-      {
-        text: "BIO5",
+      {price:360,
+        kcal:350,
         img:  "https://lays.ro/images/pack-clasic-barbecue.png",
       },
     ];
     var snack = quotes[Math.floor(Math.random() * quotes.length)];
     document.getElementById("snacks").innerHTML =
-    '<div class = "card-image">' +
       `<figure class = "image is-square">` +
-      '<img src="' + snack.img + '">' + '</figure>' 
-      '</div>'
-      '<p>' + snack.text + '</p>';
+      '<img loading ="lazy" src="' + snack.img + '">' + '</figure>' ;
+  
+      document.getElementById("descri").innerHTML = 
+      '<span>Pirce: ' + snack.price + '</span>'+ '<br>' +
+      '<span>Kcal: ' + snack.kcal+ '</span>';
+     
+
   }
+
+
+ 
